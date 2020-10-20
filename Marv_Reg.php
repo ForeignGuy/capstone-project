e0bf48928b22d5048046bafe0ac0da9e534ae617
mdlaubstein <mdlaubstein@oakland.edu>
2020-10-20 18:22:37 -0400

    Allowed homepage to be visited from all other pages.

--- Marv_Reg.php
+++ Marv_Reg.php
@@ -8,43 +8,42 @@
 		<meta charset="utf-8">
 		<link rel="stylesheet" type="text/css" href="CSS/Main.css">
 	</head>
-	
 	<body>
 		<div id="Welcome_Bar"> <br>
 			<span id="Greetings" class="Center"> <h1> Welcome to Marv<?php if (isset($_SESSION['Username'])) { echo ", "; echo $_SESSION['Username'];  echo "!"; }?> </h1></span>
 			<span class="Welcome_Item"> <a href="Homepage.php"> Homepage </a> </span> 
-			<span class="Welcome_Item"> About Marv </span> 
+            <span class="Welcome_Item"> <a href="About_Marv.php"> About Marv </a> </span>
 			<span class="Welcome_Item"> <a href="About_Us.php"> About Us </a> </span> 
 			<span class="Welcome_Item"> <a href="Use_Marv.php"> Use Marv </a> </span> 
 			<span Id="Final_Welcome_Item"> <a href="Contact_Us.php"> Contact Us </a> </span>
 		</div>
         
-        <div>
+        <div align='center'>
         <?php 
 				if (!isset($_SESSION['Username'])) { echo "
 					<form method='POST' action='PHP_Actions/Login_Or_Register.php'> 
-						<h1 id='Login_Title'> Register </h1> 
-							<label for='Email_Address'> Email Addess: </label>
-							<input type='text' name='Email_Address' id='Email_Address' size='75' required>
+						<br><h1> Register </h1> <br><br>
+							<label for='Email_Address'> Email Address </label> <br>
+							<input placeholder='Email Address' type='text' name='Email_Address' id='Email_Address' size='75' required>
 							
 							<br><br>
 							
-							<label for='Username'> Username: </label>
-							<input type='text' name='Username' id='Username' size='75' required>
-							
-							<br><br>
-                            
-                            <label for='Password'> Password: </label>
-							<input type='text' name='Password' id='Password' size='75' required>
+							<label style='text-size: 50' for='Username'> Username </label> <br>
+							<input placeholder='Username' type='text' name='Username' id='Username' size='75' required>
 							
 							<br><br>
                             
-                            <label for='Phone_Number'> Phone Number: </label>
-							<input type='text' name='Phone_Number' id='Phone_Number' size='75' optional>
+                            				<label for='Password'> Password </label> <br>
+							<input placeholder='Password' type='text' name='Password' id='Password' size='75' required>
 							
 							<br><br>
+                            
+                            				<label for='Phone_Number'> Phone Number </label> <br>
+							<input placeholder='(Optional)' type='text' name='Phone_Number' id='Phone_Number' size='75' optional>
+							
+							<br><br><br>
 
-							<input type='submit' id='Register' name='Register' value='Register'>
+							<button style='width: 100; height: 35' type='submit' id='Register' name='Register' value='Register'> Register </button>
 					</form>";
 				} else {
 					echo "<div class='Login_Or_Logout_Area Float_Right'> 
