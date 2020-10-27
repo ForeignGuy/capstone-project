11c539477b7054567b0ef046fd314c6c14fad0b6
mdlaubstein <mdlaubstein@oakland.edu>
2020-10-27 12:49:13 -0400

    Registration now updates both the user and user_account_type tables, thereby storing all user information as well as the fact that said users will not be administrators.

--- Marv_Reg.php
+++ Marv_Reg.php
@@ -4,6 +4,7 @@
 
 <html>
 	<head> 
+		<title> About Marv </title>
 		<title> Registration </title>
 		<meta charset="utf-8">
 		<link rel="stylesheet" type="text/css" href="CSS/Main.css">
@@ -21,7 +22,7 @@
         <div align='center'>
         <?php 
 				if (!isset($_SESSION['Username'])) { echo "
-					<form method='POST' action='PHP_Actions/Login_Or_Register.php'> 
+					<form method='POST' action='PHP_Actions/Login_Action.php'> 
 						<br><h1> Register </h1> <br><br>
 							<label for='Email_Address'> Email Address </label> <br>
 							<input placeholder='Email Address' type='text' name='Email_Address' id='Email_Address' size='75' required>
@@ -33,12 +34,12 @@
 							
 							<br><br>
                             
-                            				<label for='Password'> Password </label> <br>
+							<label for='Password'> Password </label> <br>
 							<input placeholder='Password' type='text' name='Password' id='Password' size='75' required>
 							
 							<br><br>
                             
-                            				<label for='Phone_Number'> Phone Number </label> <br>
+							<label for='Phone_Number'> Phone Number </label> <br>
 							<input placeholder='(Optional)' type='text' name='Phone_Number' id='Phone_Number' size='75' optional>
 							
 							<br><br><br>
--- PHP_Actions/Login_Action.php
+++ PHP_Actions/Login_Action.php
@@ -4,6 +4,8 @@
 	
 	$UserUsername = $_POST['Username'];
 	$UserPassword = $_POST['Password'];
+	$UserEmail = $_POST['Email_Address'];
+	$UserPhone = $_POST['Phone_Number'];
 	$currentUsername = $_SESSION['Username'];
 	
 	if (isset($_POST['Login'])) {
@@ -17,10 +19,10 @@
 	
 	} else if (isset($_POST['Register'])) {
 		if (mysqli_num_rows(mysqli_query($connection, "SELECT * FROM `users` WHERE (Username = $UserUsername)")) < 1) {
-			mysqli_query($connection, "INSERT INTO `users` (Username, Password) VALUES ('$UserUsername', '$UserPassword')");
+			mysqli_query($connection, "INSERT INTO `users` (Username, Password, Email_Address, Phone_Number) VALUES ('$UserUsername', '$UserPassword','$UserEmail', '$UserPhone')");
 			
-			$currentLogin = mysqli_query($connection, "SELECT Username FROM `users` WHERE Username=$currentUsername");
-			mysqli_query($connection, "INSERT INTO `user_account_type` (Users_User_Number, Admin_Account) VALUES ($currentLogin, 0)");
+			$lastInsertedId = mysqli_insert_id($connection);
+			mysqli_query($connection, "INSERT INTO `user_account_type` (Users_User_Number, Admin_Account) VALUES ($lastInsertedId, 0)");
 			header("Location: ../Homepage.php");
 		} else {
 			echo '<script type="text/javascript"> alert("That username already exists! Try another one."); </script>';
@@ -31,5 +33,6 @@
 		echo '<script type="text/javascript"> alert("We cannot tell if you were registering or logging in. Please contact us and notify us about this issue."); </script>';
 		echo '<script type="text/javascript"> window.location.href="../Homepage.php" </script>';
 	}
+
 
 ?>
