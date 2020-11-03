7fc495fd0e2f48cd076f2cee01230e8491f12315
JacobBaum2 <71450211+JacobBaum2@users.noreply.github.com>
2020-11-02 23:22:30 -0500

    Updated MavTest.py
    New update for the python file with a host of new testing features. Also a new csv file with more articles. Should all be in php_actions

--- PHP_Actions/MarvTest.py
+++ PHP_Actions/MarvTest.py
@@ -8,52 +8,76 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import PassiveAggressiveClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix
 
-
+# the default configuration i'm pushing this code in is to calculate an accuracy percentage between two
+# unrelated data sets. It can do the other tests by uncommenting code blocks
 # Read the data
-df = pd.read_csv(r"news.csv")
-df1 = pd.read_csv(r"userInput.csv")
+# 40,000 news articles
+df = pd.read_csv(r"theOmega.csv")
+# original data of 7,000
+df1 = pd.read_csv(r"news.csv")
 
 df1.shape
 df1.head()
-
-labels1 = df1.label
-labels1.head()
 
 # Get shape
 df.shape
 df.head()
 
-# DataFlair Labels
-labels = df.label
-labels.head()
+# find and replace code to get rid of a few phrases in the beeg data set. Dont worry about this it works great
+dfm = [df['text'][i].replace('WASHINGTON (Reuters) - ', '').replace('LONDON (Reuters) - ', '').replace('(Reuters) - ', '') for i in range(len(df['text']))]
+df['text'] = dfm
 
-# Setting up sets
-x_train, x_test, y_train, y_test = train_test_split(df['text'], labels, test_size=0.8, random_state=1165)
-x_input = labels1
-# print(x_test)
+# piece of test code to print the first entry in the test. If your using userInput.csv, it'll print the user input
+# print(df1['text'][0])
+
+# this is commented out placeholder code for splitting the dataset, don't use if your using the below code
+# x_train,x_test,y_train,y_test=train_test_split(df['text'], df['label'], test_size=0.5, random_state=7, shuffle=True)
+
+# new xtrain and ytrain variable definitions. The dataset is not split using these, don't use if you're splitting
+# the dataset
+x_train = df['text']    # df text refers to the text column of the dataframe defined above
+y_train = df['label']   # df label refers to the label column of the dataframe defined above
 
 # Initialize TfidVectorizer
 tfidf_vectorizer = TfidfVectorizer(stop_words='english', max_df=0.7)
 
 # Fit and transform sets
 tfidf_train = tfidf_vectorizer.fit_transform(x_train)
-tfidf_test = tfidf_vectorizer.transform(x_test)
-tfidf1_test = tfidf_vectorizer.transform(x_input)
+tfidf_test = tfidf_vectorizer.transform(df1['text'])
+# use this code line below if your using the split data set, comment out the one above if you are
+# tfidf_test = tfidf_vectorizer.transform(x_test)
 
 # Initialize Passive Aggressive Classifier
 pac = PassiveAggressiveClassifier(max_iter=50)
 pac.fit(tfidf_train, y_train)
 
-# Predict on test set and calc accuracy
-y_pred = pac.predict(tfidf_test)
-print(y_pred)
-scoredefault = pac.decision_function(tfidf_test)
-print(scoredefault)
-score = accuracy_score(y_test, y_pred)
-print(f'Accuracy: {round(score*100,2)}%')
+# the code block below is for the different outputs you can get
+# the pac.predict lines are for prediciting and there are the different things you can print out beneath it
+# its currently commented out because i was testing percent accuracy
 
-resultPred = pac.predict(tfidf1_test)
-print(resultPred)
-score1 = pac.decision_function(tfidf1_test)
-print(score1)
-print(x_input)
+# y_pred = pac.predict(tfidf_test)
+# print(y_pred)
+# scoredefault = pac.decision_function(tfidf_test)
+# print(scoredefault)
+
+# score = accuracy_score(y_test,y_pred)
+# print(f'Accuracy: {round(score*100,2)}%')
+
+# this is a function i stole from online and modified to do what i want
+# it should make a vector of the new input and run the pac on it
+# i also stole the if statement uncreditly for testing purposes
+# should compare the label it just predicted with the label thats actually in df1 and assign a one if they're the same
+# then all the ones are summed and divided by the size of df1 to get the accuracy percentage
+# if you guys see an error in this testing methodology please let me know or change it because it is the basis of
+# my results
+
+
+def findlabel(newtext):
+    vec_newtest=tfidf_vectorizer.transform([newtext])
+    y_pred1 = pac.predict(vec_newtest)
+    return y_pred1[0]
+
+
+percent = sum([1 if findlabel((df1['text'][i])) == df1['label'][i] else 0 for i in range(len(df1['text']))])/df1['text'].size
+print(percent)
+
