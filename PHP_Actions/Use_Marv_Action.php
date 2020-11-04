8d3834a318d77f4ebc71a2525a11f43d400fb342
zaidbale1996 <71236284+zaidbale1996@users.noreply.github.com>
2020-11-03 20:48:40 -0500

    Update Use_Marv_Action.php

--- PHP_Actions/Use_Marv_Action.php
+++ PHP_Actions/Use_Marv_Action.php
@@ -1,5 +1,7 @@
-<?php 
-$command = escapeshellcmd('MarvTest.py');
-$output = shell_exec($command);
-echo $output;
+<?php
+
+    $Userinput=isset($_POST['Input_Headline']) ? $_POST['Input_Headline'] : "";
+    $command = escapeshellcmd("MarvTest.py $Userinput");
+    $output = shell_exec($command);
+    echo $output;
 ?>
