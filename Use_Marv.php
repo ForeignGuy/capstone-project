9847b769c0d3c56824c047490c4cb722949cfce1
zaidbale1996 <71236284+zaidbale1996@users.noreply.github.com>
2020-11-03 20:47:49 -0500

    Update Use_Marv.php

--- Use_Marv.php
+++ Use_Marv.php
@@ -20,18 +20,14 @@
 
 			<?php
 				if (!isset($_SESSION['Username'])) { echo "
-					<form class='Login_Or_Logout_Area Float_Right' method='POST' action='PHP_Actions/Login_Or_Register.php'> 
-						<h4 id='Login_Title'> Sign-in/Register </h4> 
+					<form class='Login_Or_Logout_Area Float_Right' method='POST' action='PHP_Actions/Login_Or_Register.php'>
+						<h4 id='Login_Title'> Sign-in/Register </h4>
 							<label for='Username' class='UsernameAndPassword'> Username: </label>
 							<input type='text' name='Username' id='Username' required>
-							
 							<br><br>
-							
 							<label for='Password' class='UsernameAndPassword'> Password: </label>
 							<input type='text' name='Password' id='Password' required>
-							
 							<br>
- 
 							<input type='submit' id='Login' class='Login_And_Register_Buttons Float_Right' name='Login' value='Login'>
 					</form>
 					<br>
@@ -41,11 +37,8 @@
 							<h4 id='Logout_Title'> Log Out </h4>
 						  </div>";
 				}
-				
-			if(!isset($_SESSION['Username'])) {
-				echo '<script type="text/javascript">alert("Login is required to view page contents");</script>';
-					 exit();
-           }
+
+
 			?>
 			<form id="Logout_Form" class="Float_Right" method="POST" action="PHP_Actions/Logout.php"> <br><br><br><br>
 				<?php if (isset($_SESSION['Username'])) { echo "<input type='submit' id='Logout_Button' name='Logout' value='Log Out'>"; } ?>
@@ -53,7 +46,12 @@
 		</div>
 
 		<form method='POST' action="PHP_Actions/Use_Marv_Action.php">
+			<label for="Use_Marv">Input_Headline:</label>
+			<br>
+			<textarea id="Use_Marv" name="Use_Marv" rows="5" cols="75" placeholder="Please search here"></textarea>
+			<br>
 			<input type="submit" value="Use Marv">
+
 		</form>
 	</body>
 </html>
