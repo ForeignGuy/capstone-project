dd389a01dd80a82c80c19ccae3bb0770ce6146e6
zaidbale1996 <71236284+zaidbale1996@users.noreply.github.com>
2020-11-03 20:56:11 -0500

    Update Use_Marv.php

--- Use_Marv.php
+++ Use_Marv.php
@@ -18,16 +18,21 @@
 			<span class="Welcome_Item"> Use Marv </span>
 			<span id="Final_Welcome_Item"> <a href="Contact_Us.php"> Contact Us </a> </span>
 
+
 			<?php
 				if (!isset($_SESSION['Username'])) { echo "
 					<form class='Login_Or_Logout_Area Float_Right' method='POST' action='PHP_Actions/Login_Or_Register.php'>
 						<h4 id='Login_Title'> Sign-in/Register </h4>
 							<label for='Username' class='UsernameAndPassword'> Username: </label>
 							<input type='text' name='Username' id='Username' required>
+
 							<br><br>
+
 							<label for='Password' class='UsernameAndPassword'> Password: </label>
 							<input type='text' name='Password' id='Password' required>
+
 							<br>
+
 							<input type='submit' id='Login' class='Login_And_Register_Buttons Float_Right' name='Login' value='Login'>
 					</form>
 					<br>
@@ -37,7 +42,10 @@
 							<h4 id='Logout_Title'> Log Out </h4>
 						  </div>";
 				}
-
+				if(!isset($_SESSION['Username'])) {
+								echo '<script type="text/javascript">alert("Login is required to view page contents");</script>';
+									 exit();
+				           }
 
 			?>
 			<form id="Logout_Form" class="Float_Right" method="POST" action="PHP_Actions/Logout.php"> <br><br><br><br>
