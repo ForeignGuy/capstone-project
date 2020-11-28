60d070fc21c40339f9593692482582ece9086198
mdlaubstein <mdlaubstein@oakland.edu>
2020-11-27 20:29:41 -0500

    Fixed the login to work with hashed passwords; used Zaid's old code.

--- PHP_Actions/Login_Or_Register.php
+++ PHP_Actions/Login_Or_Register.php
@@ -2,31 +2,35 @@
 	session_start();
 	$connection = mysqli_connect("localhost", "root", "", "Marv_Related_Information");
 	
-	$UserUsername = $_POST['Username'];
-	$UserPassword = $_POST['Password'];
-	$UserEmail = $_POST['Email_Address'];
-	$UserPhone = $_POST['Phone_Number'];
-	
 	if (isset($_POST['Login'])) {
-		if (mysqli_num_rows(mysqli_query($connection, "SELECT * FROM `users` WHERE (Username = '$UserUsername') AND (Password = '$UserPassword')")) > 0) {
+		$UserUsername = $_POST['Username'];
+		$UserPassword = $_POST['Password'];
+		$hashpass2 = $_SESSION['hash'];
+		if (mysqli_num_rows(mysqli_query($connection, "SELECT * FROM `Users` WHERE (Username = '$UserUsername') AND (Password = '$hashpass2')")) > 0) {
 			$_SESSION['Username'] = $UserUsername;
 			header("Location: ../Homepage.php");
 		} else {
-			echo '<script type="text/javascript"> alert("Your login information does not match any existing records."); </script>';
-			echo '<script type="text/javascript"> window.location.href="../Homepage.php" </script>';
+			echo '<script type="text/javascript"> alert("No matching file for that username."); </script>';
+			echo '<script type="text/javascript"> window.location.href="../Marv_Reg.php" </script>';
 		}
-	
+
 	} else if (isset($_POST['Register_Submit'])) {
-		if (mysqli_num_rows(mysqli_query($connection, "SELECT * FROM `users` WHERE (Username = '$UserUsername')")) < 1) {
-			mysqli_query($connection, "INSERT INTO `users` (Username, Password, Email_Address, Phone_Number) VALUES ('$UserUsername', '$UserPassword','$UserEmail','$UserPhone')");
+		$UserUsername = $_POST['Username'];
+		$UserPassword = $_POST['Password'];
+		$hashpass = password_hash($UserPassword, PASSWORD_DEFAULT);
+		$UserEmail = $_POST['Email_Address'];
+		$UserPhone = $_POST['Phone_Number'];
+		if (mysqli_num_rows(mysqli_query($connection, "SELECT * FROM `Users` WHERE (Username = '$UserUsername')")) < 1) {
+			mysqli_query($connection, "INSERT INTO `Users` (Username, Password, Email_Address, Phone_Number) VALUES ('$UserUsername', '$hashpass','$UserEmail','$UserPhone')");
 			$CurrentUserNumber = mysqli_insert_id($connection);
 			mysqli_query($connection, "INSERT INTO `user_account_type` (Users_User_Number, Admin_Account) VALUES ($CurrentUserNumber, 0)");
+			$_SESSION['hash'] = $hashpass;
 			header("Location: ../Homepage.php");
 		} else {
 			echo '<script type="text/javascript"> alert("That username already exists! Try another one."); </script>';
 			echo '<script type="text/javascript"> window.location.href="../Marv_Reg.php" </script>';
 		}
-	
+
 	} else {
 		echo '<script type="text/javascript"> alert("We cannot tell if you were registering or logging in. Please contact us and notify us about this issue."); </script>';
 		echo '<script type="text/javascript"> window.location.href="../Homepage.php" </script>';
