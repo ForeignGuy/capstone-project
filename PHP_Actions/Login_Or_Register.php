2944c891d84d58ddaf6b0026b315876d3156e6c2
mdlaubstein <mdlaubstein@oakland.edu>
2020-11-28 13:31:52 -0500

    Registration hashing now permanently saves password.

--- PHP_Actions/Login_Or_Register.php
+++ PHP_Actions/Login_Or_Register.php
@@ -2,29 +2,44 @@
 	session_start();
 	$connection = mysqli_connect("localhost", "root", "", "Marv_Related_Information");
 	
+	if (isset($UserPassword)) {
+		$UserPassword = $_POST['Password'];
+
+	} else {
+		$UserPassword = null;
+	}
+	
+	
 	if (isset($_POST['Login'])) {
 		$UserUsername = $_POST['Username'];
 		$UserPassword = $_POST['Password'];
-		$hashpass2 = $_SESSION['hash'];
-		if (mysqli_num_rows(mysqli_query($connection, "SELECT * FROM `Users` WHERE (Username = '$UserUsername') AND (Password = '$hashpass2')")) > 0) {
-			$_SESSION['Username'] = $UserUsername;
-			header("Location: ../Homepage.php");
-		} else {
-			echo '<script type="text/javascript"> alert("No matching file for that username."); </script>';
-			echo '<script type="text/javascript"> window.location.href="../Marv_Reg.php" </script>';
-		}
+		$UserPasswordResultsObject = mysqli_query($connection, "SELECT `Password` FROM `users` WHERE Username='$UserUsername'");
+		while ($row = mysqli_fetch_assoc($UserPasswordResultsObject)) {
+			$DatabasePassword = $row["Password"];
+			
+			$UserPassword = trim($UserPassword);
+			$DatabasePassword = trim($DatabasePassword);
+			
+			$checkmatch = password_verify($UserPassword, $DatabasePassword);
+			if (mysqli_num_rows(mysqli_query($connection, "SELECT * FROM `Users` WHERE (Username = '$UserUsername') AND ('$checkmatch'=1)")) > 0) {
+				$_SESSION['Username'] = $UserUsername;
+				header("Location: ../Homepage.php");
+			} else {
+				echo '<script type="text/javascript"> alert("Username or passowrd not found."); </script>';
+				echo '<script type="text/javascript"> window.location.href="../Marv_Reg.php" </script>';
+			}
 
+		}
 	} else if (isset($_POST['Register_Submit'])) {
 		$UserUsername = $_POST['Username'];
 		$UserPassword = $_POST['Password'];
-		$hashpass = password_hash($UserPassword, PASSWORD_DEFAULT);
 		$UserEmail = $_POST['Email_Address'];
 		$UserPhone = $_POST['Phone_Number'];
+		$hashpass = password_hash($UserPassword, PASSWORD_DEFAULT);
 		if (mysqli_num_rows(mysqli_query($connection, "SELECT * FROM `Users` WHERE (Username = '$UserUsername')")) < 1) {
 			mysqli_query($connection, "INSERT INTO `Users` (Username, Password, Email_Address, Phone_Number) VALUES ('$UserUsername', '$hashpass','$UserEmail','$UserPhone')");
 			$CurrentUserNumber = mysqli_insert_id($connection);
 			mysqli_query($connection, "INSERT INTO `user_account_type` (Users_User_Number, Admin_Account) VALUES ($CurrentUserNumber, 0)");
-			$_SESSION['hash'] = $hashpass;
 			header("Location: ../Homepage.php");
 		} else {
 			echo '<script type="text/javascript"> alert("That username already exists! Try another one."); </script>';
