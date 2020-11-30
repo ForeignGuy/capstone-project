678f1ee6d381271772ec1fecc39167450e0aa03e
tovelbowie6 <71236283+tovelbowie6@users.noreply.github.com>
2020-11-29 20:20:11 -0500

    Updating the path for the CDN

--- Homepage.php
+++ Homepage.php
@@ -9,7 +9,7 @@
 		<meta charset="utf-8">
   <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,600,700,800" rel="stylesheet" />
   <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet" />
-  <link href="./assets/css/blk-design-system.css" rel="stylesheet" />
+  <link href="./css/blk-design-system.css" rel="stylesheet" />
 	</head>
 	
 	<body>
